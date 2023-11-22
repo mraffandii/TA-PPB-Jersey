8aafb02f0876c5f472245d5f881dd14494b0faa3
Mohammad Rizki Affandi <mohammadrizkiaffandi@gmail.com>
2023-11-22 18:37:03 +0700

    commit

---
+++ src/pages/about/About.js
@@ -0,0 +1,20 @@
+import './About.css';
+
+export default function About() {
+  return (
+    <div className="about">
+      <div className="about-content">
+        <img src="https://media.licdn.com/dms/image/D5603AQHjAE_b5S1FeA/profile-displayphoto-shrink_400_400/0/1690956340366?e=1706140800&v=beta&t=bX3Rkq4SXqJhSBFk1_xyB9hgNeImo4b0lHRjDHN5SLc" alt="icon" />
+        <h3>Jerseycatalogue</h3>
+        <p>
+          Jerseycatalogue adalah website yang menyediakan informasi 
+          tentang klub sepak bola dan jerseynya. Website ini dibuat 
+          untuk memenuhi tugas akhir Praktikum Pemrograman Perangkat Bergerak.
+          <p>
+          Created by: Mohammad Rizki Affandi - 21120121140159
+          </p>
+        </p>
+      </div>
+    </div>
+  );
+}
